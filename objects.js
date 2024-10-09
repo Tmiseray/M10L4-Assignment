abc8c985d17737f47fd7b558692a57ee616b1636
Tmiseray <161903165+Tmiseray@users.noreply.github.com>
2024-10-08 20:56:43 -0500

    adding functions

--- objects.js
+++ objects.js
@@ -7,36 +7,45 @@ function Book(title, author, pages) {
 }
 
 Book.prototype.display = function() {
-    console.log(`Title: ${this.title} \nAuthor: ${this.author} \nPages: ${this.pages}`)
+    console.log(`Title: ${this.title} \nAuthor: ${this.author} \nPages: ${this.pages}`);
 }
 
-function addBook(arr, bookArr) {
-    arr.push([Book.title]);
+function addBook(arr, i, title, author, pages) {
+    let i = new Book(title, author, pages);
+    arr.push(i);
     return arr;
 }
 
 function bookByTitle(arr, titleQuery) {
-    let book = arr.filter(Book.title.titleQuery);
-    if (book) {
-        return `Title: ${book.title} \nAuthor: ${book.author} \nPages: ${book.pages}`;
-    } else {
-        return `Title: ${titleQuery} not found in library`;
-    }
+    arr.filter(function(book) {
+        if (book.title == titleQuery) {
+            return `Title: ${book.title} \nAuthor: ${book.author} \nPages: ${book.pages}`;
+        } else {
+            return `Title: ${titleQuery} not found in library`;
+        }
+    });
 }
 
 function bookByAuthor(arr, authorQuery) {
-    let book = arr.filter(Book.author.authorQuery);
-    if (book) {
-        return `Title: ${book.title} \nAuthor: ${book.author} \nPages: ${book.pages}`;
-    } else {
-        return `Title: ${authorQuery} not found in library`;
-    }
+    arr.filter(function(book) {
+        if (book.author == authorQuery) {
+            return `Title: ${book.title} \nAuthor: ${book.author} \nPages: ${book.pages}`;
+        } else {
+            return `Author: ${authorQuery} not found in library`;
+        }
+    });
 }
 
 function filterPages(arr) {
-
+    arr.filter(function(book) {
+        if (book.pages >= 100) {
+            return `Title: ${book.title} \nAuthor: ${book.author} \nPages: ${book.pages}`;
+        } else {
+            return "Books with 100 or more pages not found in library";
+        }
+    });
 }
 
 
-Book("title", "author", 100);
+
 
